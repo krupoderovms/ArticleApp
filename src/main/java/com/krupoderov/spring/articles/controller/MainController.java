6f99f14bdabc1ca84999537e88c9ce77fabd28ae
KrupoderovMikhail <mishka.crash@gmail.com>
2019-05-19 22:10:12 +0500

    Code refactor.

--- src/main/java/com/krupoderov/spring/articles/controller/MainController.java
+++ src/main/java/com/krupoderov/spring/articles/controller/MainController.java
@@ -3,12 +3,18 @@ package com.krupoderov.spring.articles.controller;
 import com.krupoderov.spring.articles.model.Article;
 import com.krupoderov.spring.articles.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/") // адрес, по которому будет вызван контроллер
@@ -39,6 +45,15 @@ public class MainController {
     public String deleteArticle(@PathVariable("article_id") Integer articleId) {
         service.delete(articleId);
         return "redirect:/";
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
     }
 
     @RequestMapping(value = "/login")
