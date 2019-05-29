48f4c5c446856c5f30914025b2d704d60aabe0b7
KrupoderovMikhail <mishka.crash@gmail.com>
2019-05-30 00:25:50 +0500

    Added password encryption.

--- src/main/java/com/krupoderov/spring/articles/configuration/WebSecurityConfig.java
+++ src/main/java/com/krupoderov/spring/articles/configuration/WebSecurityConfig.java
@@ -2,12 +2,14 @@ package com.krupoderov.spring.articles.configuration;
 
 import com.krupoderov.spring.articles.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +17,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder(8);
+    }
 
     @Override
     protected void configure(HttpSecurity config) throws Exception {
@@ -36,6 +46,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .userDetailsService(userService)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+                .passwordEncoder(passwordEncoder);
     }
 }
--- src/main/java/com/krupoderov/spring/articles/controller/RegistrationController.java
+++ src/main/java/com/krupoderov/spring/articles/controller/RegistrationController.java
@@ -4,6 +4,7 @@ import com.krupoderov.spring.articles.model.Role;
 import com.krupoderov.spring.articles.model.User;
 import com.krupoderov.spring.articles.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,9 @@ public class RegistrationController {
 
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @GetMapping("/registration")
     public String registration() {
@@ -33,6 +37,7 @@ public class RegistrationController {
 
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepo.save(user);
 
         return "redirect:/login";
