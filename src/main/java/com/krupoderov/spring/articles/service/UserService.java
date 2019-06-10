9449fda08d7f0ecd5e9ff1be0c9cf8d1eeef97cf
KrupoderovMikhail <mishka.crash@gmail.com>
2019-06-11 00:08:52 +0500

    Delete migration.

--- src/main/java/com/krupoderov/spring/articles/service/UserService.java
+++ src/main/java/com/krupoderov/spring/articles/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -18,6 +19,9 @@ import org.springframework.util.StringUtils;
  */
 @Service
 public class UserService implements UserDetailsService {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Autowired
     private UserRepo userRepo;
@@ -35,7 +39,7 @@ public class UserService implements UserDetailsService {
 
     public void updateProfile(User user, String password) {
         if (!StringUtils.isEmpty(password)) {
-            user.setPassword(password);
+            user.setPassword(passwordEncoder.encode(password));
         }
 
         userRepo.save(user);
