d6ee8c39ffa52f4fc011717f74c1e8ad7b846fad
Michael <41121626+krupoderovmikhail@users.noreply.github.com>
2019-10-12 18:29:58 +0500

    Added a class containing constants commonly used in entities.

---
+++ src/main/java/com/krupoderov/spring/articles/utils/Constants.java
@@ -0,0 +1,22 @@
+package com.krupoderov.spring.articles.utils;
+
+public class Constants {
+    public static final int USERNAME_LENGTH_MIN = 3;
+    public static final int USERNAME_LENGTH_MAX = 20;
+
+    public static final int PASSWORD_LENGTH_MIN = 4;
+    public static final int PASSWORD_LENGTH_MAX = 20;
+
+    public static final int TITLE_LENGTH_MIN = 5;
+    public static final int TITLE_LENGTH_MAX = 255;
+
+    public static final int CONTENT_LENGTH_MIN = 5;
+    public static final int CONTENT_LENGTH_MAX = 2000;
+
+    public static final int PHOTO_WIDTH = 100;
+    public static final int PHOTO_HEIGHT = 100;
+    public static final String PHOTO_DEFAULT = "/images/user_no_image.png";
+
+    public static final int PAGE_DEFAULT_SIZE = 5;
+
+}
