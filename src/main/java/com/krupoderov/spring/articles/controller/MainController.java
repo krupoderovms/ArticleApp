3265fa3949d46d1af7970576a06520e53e33e373
Michael <41121626+krupoderovmikhail@users.noreply.github.com>
2019-10-20 16:24:33 +0500

    Redesigned Homepage

--- src/main/java/com/krupoderov/spring/articles/controller/MainController.java
+++ src/main/java/com/krupoderov/spring/articles/controller/MainController.java
@@ -1,14 +1,11 @@
 package com.krupoderov.spring.articles.controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import com.krupoderov.spring.articles.repository.SectionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * Класс, представляющий собой контроллер, в котором происходит отображение главной страницы
@@ -20,37 +17,14 @@ import javax.servlet.http.HttpServletResponse;
 @RequestMapping("/")
 public class MainController {
 
-    /**
-     * Главная страница
-     * @return main.html
-     */
-    @RequestMapping
-    public String mainPage() {
+    @Autowired
+    SectionRepository sectionRepository;
+
+    @GetMapping
+    public String mainPage(Model model) {
+        model.addAttribute("title", "Home page :)");
+        model.addAttribute("sections", sectionRepository.findAllByParent(null));
+
         return "main";
-    }
-
-    /**
-     * Страница авторизации
-     * @return login.html
-     */
-    @RequestMapping(value = "/login")
-    public String loginPage() {
-        return "login";
-    }
-
-    /**
-     * Страница выхода из аккаунта
-     * @param request формирует клиенту ответ c информацией о запросе, отправленным методом GET, и генерирует ответ клиенту
-     * @param response определяет ответ клиенту
-     * @return возвращает на страницу логина(логаута)
-     */
-    @RequestMapping(value = "/logout", method = RequestMethod.GET)
-    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null) {
-            new SecurityContextLogoutHandler().logout(request, response, auth);
-        }
-
-        return "redirect:/login?logout";
     }
 }
